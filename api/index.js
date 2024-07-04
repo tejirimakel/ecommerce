286b7403cd2495fd7f9393bb725357b74feb962f
Tejiri Makele <tejirimakele@yahoo.com>
2024-07-04 11:59:21 +0100

    n

--- api/index.js
+++ api/index.js
@@ -1,22 +1,20 @@
-const { join } = require("path");
-const { promisify } = require("util");
 const { exec } = require("child_process");
+const { promisify } = require("util");
 
+// Convert exec callback style to promise
 const execAsync = promisify(exec);
 
 module.exports = async (req, res) => {
   try {
-    if (req.url === "/") {
-      const filePath = join(__dirname, "..", "public", "index.php");
-      const fileContents = fs.readFileSync(filePath, "utf8");
-      res.setHeader("Content-Type", "text/html");
-      res.end(fileContents);
-    } else {
-      const phpScriptPath = "../index.php";
+    const phpScriptPath = "../index.php"; // Ensure this path is correct relative to the function's location
+    try {
       const { stdout } = await execAsync(`php ${phpScriptPath}`);
       res.status(200).send(stdout);
+    } catch (error) {
+      console.error(`exec error: ${error.message}`);
+      res.status(500).send({ error: "Failed to execute PHP script" });
     }
   } catch (error) {
-    res.status(500).send({ error: "Failed to serve the page" });
+    res.status(500).send({ error: "An internal server error occurred" });
   }
 };
